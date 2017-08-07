9a428f763fd89b6917fca1b6db9990c8fc6faf4b
ly321 <965768042@qq.com>
2017-08-07 20:23:03 +0800

    暂停
    因img url问题，暂停学习下webpack

---
+++ zhuishushenqi-byVue/src/components/Headertwo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="headertwo">
+        <span>
+            <img src="../assets/leftarrow.png" alt="">
+            <p>返回</p>
+        </span>
+        <p>玄幻</p>
+    </div>
+</template>
+<script>
+export default {
+  name:'headertwo',
+  data(){
+      return{
+      }
+  }
+}
+</script>
+
+<style>
+img{
+    width: 16px;
+    height: 16px;
+}
+</style>
--- zhuishushenqi-byVue/src/pages/List.vue
+++ zhuishushenqi-byVue/src/pages/List.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="list">
-        1111111111
+        <Headertwo></Headertwo>
     </div>
 </template>
 <script>
+import Headertwo from '../components/Headertwo'
 export default {
   name:'list',
   data(){
       return{
 
       }
+  },
+  components:{
+      Headertwo,
   }
 }
 </script>
